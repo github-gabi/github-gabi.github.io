513f60aa15e1a8115991bdd19ae8641d3c13fef4
gabi-github <g_bartalis@yahoo.com>
2018-10-21 23:39:11 +0300

    upload

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+<script>
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("toTop").style.display = "block";
+    } else {
+        document.getElementById("toTop").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
